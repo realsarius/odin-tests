9bd137da51aee235c1fa87c8c646fc2b4eefb70f
realsarius <berkansozer@protonmail.com>
2023-03-10 20:34:03 +0300

    completed the project

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 
-const reverseString = (string, reversedString = '') => {
-  for (let i = string.length - 1; i >= 0; i -= 1) {
-    reversedString += string[i];
-  }
-  return reversedString;
+// const reverseString = (string, reversedString = '') => {
+//   for (let i = string.length - 1; i >= 0; i -= 1) {
+//     reversedString += string[i];
+//   }
+//   return reversedString;
+// };
+
+const reverseString = (string, num = string.length, tempString = '') => {
+  if (num < 0) return tempString;
+  tempString += string.charAt(num);
+  return reverseString(string, num - 1, tempString);
 };
+
+console.log(reverseString('berkan'));
 
 const calculator = {
   add(x, y) {
@@ -25,4 +33,46 @@ const calculator = {
   },
 };
 
-module.exports = { reverseString, capitalize, calculator };
+const caesarCipher = (str, key) =>
+  str
+    .toUpperCase()
+    .replace(/[A-Z]/g, (c) =>
+      String.fromCharCode(((c.charCodeAt(0) - 65 + key) % 26) + 65)
+    );
+
+const analyzeArray = {
+  average(arr, sum = 0) {
+    arr.forEach((num) => {
+      sum += num;
+    });
+    return Math.floor(sum / arr.length);
+  },
+
+  min(arr) {
+    let min = arr[0];
+    arr.forEach((num) => {
+      if (num < min) min = num;
+    });
+    return min;
+  },
+
+  max(arr) {
+    let max = arr[0];
+    arr.forEach((num) => {
+      if (num > max) max = num;
+    });
+    return max;
+  },
+
+  length(arr) {
+    return arr.length;
+  },
+};
+
+module.exports = {
+  reverseString,
+  capitalize,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+};
--- app.test.js
+++ app.test.js
@@ -4,13 +4,38 @@ test('reversed the string: elizabeth', () => {
   expect(app.reverseString('elizabeth')).toBe('htebazile');
 });
 
-test('capitalize the first letter function: berkan', () => {
-  expect(app.capitalize('berkan')).toBe('Berkan');
+test('capitalize the first letter function: nolan', () => {
+  expect(app.capitalize('nolan')).toBe('Nolan');
 });
 
 test('calculator test: add, subtract, multiply and divide 24 and 6', () => {
-  expect(app.calculator.add(24, 6)).toBe(30);
-  expect(app.calculator.subtract(24, 6)).toBe(18);
-  expect(app.calculator.multiply(24, 6)).toBe(144);
-  expect(app.calculator.divide(24, 6)).toBe(4);
+  const x = 24;
+  const y = 6;
+  expect(app.calculator.add(x, y)).toBe(30);
+  expect(app.calculator.subtract(x, y)).toBe(18);
+  expect(app.calculator.multiply(x, y)).toBe(144);
+  expect(app.calculator.divide(x, y)).toBe(4);
+});
+
+test('ceaser cipher as usual', () => {
+  expect(app.caesarCipher('elizabeth!', 1)).toBe('fmjabcfui!'.toUpperCase());
+  expect(app.caesarCipher('red wheelbarrow bbq', 6)).toBe(
+    'xkj cnkkrhgxxuc hhw'.toUpperCase()
+  );
+  expect(
+    app.caesarCipher(
+      'gurcreevacntrfjvyyuryclbhsvaqlbhepnyyvatohgqbagorqhcrqphgqbjagurjbbqfgurlorreqbf',
+      13
+    )
+  ).toBe(
+    'theperrinpageswillhelpyoufindyourcallingbutdontbedupedcutdownthewoodstheybeerdos'.toUpperCase()
+  );
+});
+
+test('finds average, min, max and length of an array of numbers', () => {
+  const arr = [1, 8, 3, 4, 2, 6];
+  expect(app.analyzeArray.average(arr)).toBe(4);
+  expect(app.analyzeArray.min(arr)).toBe(1);
+  expect(app.analyzeArray.max(arr)).toBe(8);
+  expect(app.analyzeArray.length(arr)).toBe(6);
 });
